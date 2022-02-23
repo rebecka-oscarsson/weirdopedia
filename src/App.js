021a965e13856c3a60f4eb36934744e5fd9c9e39
rebecka-oscarsson <rebecka.oscarsson@gmail.com>
2022-02-23 20:00:49 +0100

    removed some states, added some props

--- src/App.js
+++ src/App.js
@@ -12,96 +12,100 @@ require("dotenv").config();
 const deepAiKey = process.env.REACT_APP_API_KEY_DEEPAI;
 
 function App() {
-  const [placeholder, setPlaceholder] = useState("type a word");
-  const [searchWord, setSearchWord] = useState("");
+  const [searchWord, setSearchWord] = useState([]);
   const [imageUrl, setImageUrl] = useState("");
   const [imageLoaded, setImageLoaded] = useState(true);
   const [articleText, setArticleText] = useState("");
-  const [articleHeadline, setArticleHeadline] = useState("");
+  const [articleHeadline, setArticleHeadline] = useState(""); //kommer från wikipedia, om det blir redirect är den ej samma som searchword
+  const [error, setError] = useState(false);
 
   //npm-paket för att deepai ska funka
   const deepai = require("deepai");
   deepai.setApiKey(deepAiKey);
 
-  
-
   //körs när articleheadline ändras, det gör den när wikipedia-api:et har hämtats klar
+  //det är bra så bilden ej hämtas om artikeln ej gör det, samt för rätt bildtext
   useEffect(() => {
     fetchImage();
   }, [articleHeadline]);
-  //vill jag ändra till searchword? Nu blir det error på allt om wikipedia ej funkar
-  //tror det är bäst så
 
   useEffect(() => {
-    if (searchWord) {
+    if (searchWord.length !== 0) {
+      setError(false);
       Fetch(
         "https://en.wikipedia.org/api/rest_v1/page/summary/" +
           searchWord +
           "?origin=*",
         //origin här är för att det inte ska bli cors-error
         (data) => {
+          console.log("wikipedia: ", data.extract);
           askAI(data.extract);
           setArticleHeadline(data.displaytitle);
-        }
+        },
+        setError
       );
     }
   }, [searchWord]);
 
-//hämtningar från deepAI
+  //hämtningar från deepAI
 
+  //det här är ett api som gör en bild från mitt sökord
   async function fetchImage() {
     if (articleHeadline) {
       let resp = await deepai.callStandardApi("text2img", {
         text: articleHeadline,
       });
-      //det här är ett api som gör en bild från mitt sökord
       setImageUrl(resp.output_url);
     }
   }
 
+  //det här är api:et som hittar på mer text utifrån en inskickad
   async function askAI(wikiText) {
-    //det här är api:et som hittar på mer text utifrån en inskickad
-    let resp = await deepai.callStandardApi("text-generator", {
-      text: wikiText,
-    });
-    //tar bort wikipedia från början av texten för den returnerar bägge efter varandra
-    const article = resp.output.substr(
-      wikiText.length,
-      wikiText.length + resp.output.length
-    );
-    console.log("wikipedia:", wikiText);
-    setArticleText(article);
+    try {
+      let resp = await deepai.callStandardApi("text-generator", {
+        text: wikiText,
+      });
+      //tar bort wikipedia från början av texten för den returnerar bägge ihopklistrade
+      const article = resp.output.substr(
+        wikiText.length,
+        wikiText.length + resp.output.length
+      );
+      setArticleText(article);
+    } catch (err) {
+      console.log("Attans, fel på AI:n! ", err);
+      setError(true);
+    }
   }
 
   return (
     <>
       <Searchbox
         setSearchWord={setSearchWord}
-        setPlaceholder={setPlaceholder}
-        placeholder={placeholder}
+        setImageUrl={setImageUrl}
         setImageLoaded={setImageLoaded}
         setArticleHeadline={setArticleHeadline}
+        articleText={articleText}
         setArticleText={setArticleText}
+        error={error}
+        imageLoaded={imageLoaded}
       />
-      {searchWord ? (
+      {(imageUrl || error) ? (
         <Image
           imageUrl={imageUrl}
-          onLoad={() => {
-            setImageLoaded(true);
-            setPlaceholder("type a word");
-          }}
+          setImageLoaded={setImageLoaded}
+          imageLoaded={imageLoaded}
           caption={articleHeadline}
+          error={error}
         />
-      ) : (
-        ""
-      )}
-      {imageLoaded ? "" : <Spinner />}
-      {articleHeadline ? (
-        <Article articleText={articleText} articleHeadline={articleHeadline} />
-      ) : (
-        ""
-      )}
-      {/* tror jag ska göra articleLoaded istället och en switch för det ska bli spinner först när man sökt */}
+      ) : null}
+      {(!imageLoaded && searchWord.length && !error) ? <Spinner /> : null}
+
+      <Article
+        articleText={articleText}
+        articleHeadline={articleHeadline}
+        error={error}
+        searchWord={searchWord}
+      />
       <Footer />
     </>
   );
