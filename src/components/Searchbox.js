d2e999a77d32aed5caa9461efd7fbe6d377285e7
rebecka-oscarsson <rebecka.oscarsson@gmail.com>
2022-02-23 20:05:17 +0100

    simplified

--- src/components/Searchbox.js
+++ src/components/Searchbox.js
@@ -1,24 +1,40 @@
 import { useState } from "react";
 
-const Searchbox = ({ setSearchWord , setPlaceholder , placeholder, setImageLoaded, setArticleHeadline, setArticleText }) => {
-
-  const [inputText, setInputText] = useState("");
+const Searchbox = ({
+  setSearchWord,
+  setImageUrl,
+  setImageLoaded,
+  setArticleHeadline,
+  setArticleText,
+  error,
+  imageLoaded
+}) => {
+  const [inputText, setInputText] = useState([]);
 
   const search = (evt) => {
     evt.preventDefault();
     setArticleHeadline(""); //för att den förra artikelns rubrik ej ska stå kvar under sökning
-    setArticleText("");
-    setSearchWord(inputText);
+    setArticleText(null);
+    // searchWord är en array med ett värde, om jag har en sträng omrenderas ej komponenterna om man söker på samma ord
+    setSearchWord([inputText]);
     setInputText("");
-    setPlaceholder("searching...");
+    setImageUrl(null);
     setImageLoaded(false); //den här gör så att det blir en spinner på bilden
   };
 
   return (
     <form onSubmit={search}>
-      <label htmlFor="searchword">Please AI tell me about the:</label>
-      <input type="text" maxLength="20" placeholder={placeholder} id="searchword" value={inputText} onChange={e => setInputText(e.target.value)} />
-      <button type="submit">?</button>
+      <label htmlFor="searchword">Type a word</label>
+      <input
+        type="text"
+        min-width="20px"
+        maxLength="20"
+        placeholder={(imageLoaded || error)? "": "searching..."}
+        id="searchword"
+        value={inputText}
+        onChange={(e) => setInputText(e.target.value)}
+      />
+      <button type="submit">look it up</button>
     </form>
   );
 };
