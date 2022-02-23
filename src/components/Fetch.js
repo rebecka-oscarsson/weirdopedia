c68ed08d037224de7406de33e01c575b33ad3e3e
rebecka-oscarsson <rebecka.oscarsson@gmail.com>
2022-02-23 20:02:41 +0100

    error handling added

--- src/components/Fetch.js
+++ src/components/Fetch.js
@@ -1,11 +1,20 @@
 //parametern callback kan vara vilken funktion som helst som man vill köra med datan
 
-function Fetch(url, callback) {
+function Fetch(url, callback, setError) {
    fetch(url)
+   .then(handleErrors)
    .then(response => response.json())
    .then(data => {
        callback(data);
-   }) 
+   })
+   .catch ((error) => {console.log("Nääjj, dä blev fel på wikipedia! ", error); setError(true)});
 }
 
 export default Fetch;
+
+function handleErrors(response) {
+    if (!response.ok) {
+        throw Error(response.statusText);
+    }
+    return response;
+}
