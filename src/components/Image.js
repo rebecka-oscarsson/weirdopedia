0e7995a59186faeeb38dfe17ff13c55c6b1c3c3e
rebecka-oscarsson <rebecka.oscarsson@gmail.com>
2022-02-23 20:04:28 +0100

    bugfix and error image

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,4 +1,34 @@
-const Image = ({ imageUrl, onLoad, caption }) => {
-  return <figure><img src={imageUrl} alt={caption} onLoad={onLoad} id="image"/><figcaption>A typical {caption} going about its business</figcaption></figure>;
-}
+const Image = ({
+  imageUrl,
+  setImageLoaded,
+  imageLoaded,
+  caption,
+  error
+}) => {
+  // let counter = 0;
+  // const imageText
+
+  let imageText = "A typical " + caption + " going about its business"
+  
+  return (error ? (
+    <figure>
+      <img
+        src="the-holler.svg"
+        alt="cartoonish drawing of a screaming person"
+        className="errorImage"
+      />
+    </figure>
+  ) : (
+    <figure>
+      <img
+        src={imageUrl}
+        alt={caption}
+        onLoad={() => setImageLoaded(true)}
+      />
+      <figcaption>{imageLoaded? imageText : null}</figcaption>
+    </figure>
+  )
+  )
+};
+
 export default Image;
