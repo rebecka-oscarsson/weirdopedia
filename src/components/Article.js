6120942fed3d734020df0534933cede870f4bb64
rebecka-oscarsson <rebecka.oscarsson@gmail.com>
2022-02-23 20:02:17 +0100

    added funny error image

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,4 +1,26 @@
-const Article = ({ articleText, articleHeadline }) => {
-  return <article><h2>Let me tell you about the {articleHeadline}:</h2>{articleText}</article>
-}
+import Spinner from "./Spinner";
+
+const Article = ({ articleText, articleHeadline, error, searchWord }) => {
+  if (error) {
+    return (
+      <article>
+        <h2>Oh no!</h2>There was an error, please try again
+      </article>
+    );
+  } else if (articleText) {
+    return (
+      <article>
+        <h2>About the {articleHeadline}:</h2>
+        {articleText}
+      </article>
+    );
+  } else if (searchWord.length)
+    return (
+      <article>
+        <Spinner />
+      </article>
+    );
+    return null //om ingen sökning gjorts
+};
+
 export default Article;
