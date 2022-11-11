ef8d3ff206e0a35ea9fdd5ff2d706db793a5e03e
rebecka-oscarsson <rebecka.oscarsson@gmail.com>
2022-11-11 17:03:00 +0100

    Change link texts

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,4 @@
 const Footer = () => {
-  return <footer>Made using <a href = "https://www.mediawiki.org/wiki/API:Main_page">MediaWiki</a>, <a href="https://deepai.org/machine-learning-model/text-generator">text generator</a> and <a href="https://deepai.org/machine-learning-model/text2img">text to image</a>  </footer>
+  return <footer>Made using <a href = "https://www.mediawiki.org/wiki/API:Main_page">MediaWiki</a>, <a href="https://deepai.org/machine-learning-model/text-generator">Text Generation API</a> and <a href="https://deepai.org/machine-learning-model/text2img">AI Image Generator API</a> </footer>
 }
 export default Footer;
